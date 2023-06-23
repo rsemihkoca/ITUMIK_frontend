554b6e785edab2e5eeb8fccb8f97a2601bd5606f
umutozturkkk <umutozturk134@gmail.com>
2023-06-23 15:00:44 +0300

    Outlined the layout page and buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './index.css';
 import { BrowserRouter as Router, Route, BrowserRouter, Routes } from "react-router-dom";
 import MainPage from './MainPage';
+import Layout from './Layout';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<MainPage />} />
+        <Route path={'/layout'} element={<Layout />} />
       </Routes>
     </BrowserRouter>
 
