4f8971134d674e36cf95fc94a1a5ed0e866ebf6c
Bünyamin Korkut <bunyaminkrkt@hotmail.com>
2023-07-16 21:15:55 +0300

    kibris ana vatana dondu

--- src/components/Floor0.js
+++ src/components/Floor0.js
@@ -7,7 +7,7 @@ import clock from '../assets/gif/Saat.gif'
 
 export const Floor0 = () => {
   return (
-    <div className="floor mx-4 rounded-xl pt-2 sm:pt-8  min-w-[240vw] sm:min-w-[1200px] my-8 sm:my-0">
+    <div className="floor mx-4 rounded-xl pt-6 sm:pt-8  min-w-[240vw] sm:min-w-[1200px] my-8 sm:my-0">
       <div className="flex justify-center gap-4 sm:gap-16">
         <img src={bookshelfFront} className="sm:w-20 w-16 col-span-1 col-start-1 col-end-1 row-span-4" />
         <img src={bookshelfFront} className="sm:w-20 w-16 col-span-1 col-start-1 col-end-1 row-span-4" />
@@ -17,7 +17,7 @@ export const Floor0 = () => {
         <img src={bookshelfFront} className="sm:w-20 w-16 col-span-1 col-start-1 col-end-1 row-span-4" />
         <img src={bookshelfFront} className="sm:w-20 w-16 col-span-1 col-start-1 col-end-1 row-span-4" />
       </div>
-      <div className="h-full px-4 grid grid-cols-11 grid-rows-5  min-w-[200vw] sm:min-w-[1200px] ">
+      <div className="h-full px-4 grid grid-cols-11 grid-rows-4 pb-5  min-w-[200vw] sm:min-w-[1200px] ">
         {/* <div className="col-span-1 col-start-1 col-end-1 row-span-4 flex items-center">
           <img src={chair} className="h-[67px] mt-12" />
           <div className="relative">
@@ -31,7 +31,7 @@ export const Floor0 = () => {
         <Table className='' />
         <Table className='' />
         <Table className='' />
-        <div className="flex justify-around items-center row-span-4">
+        <div className="flex justify-around items-center row-span-3">
           <div className="flex-col h-full py-8 justify-start relative flex ">
             <img src={bookshelfSide} className=" sm:w-5 " />
             <img src={bookshelfSide} className=" translate-y-3/4 absolute sm:w-5 " />
@@ -68,13 +68,13 @@ export const Floor0 = () => {
         <Table className='' />
         <Table className='' />
         <Table className='' />
-        <Table className='col-start-8' />
-        <Table className='' />
-        <Table className='' />
-        <Table className='' />
-        <Table doubleChair className=' col-start-4' />
+        <Table doubleChair className='col-start-4' />
         <Table doubleChair className='' />
         <Table doubleChair className='' />
+        <Table className='' />
+        <Table className='' />
+        <Table className='' />
+        <Table className='' />
         <Table className='' />
       </div>
     </div>)
--- src/components/table.js
+++ src/components/table.js
@@ -23,14 +23,14 @@ const Table = ({ doubleChair = false, c1 = false, c2 = true, c3 = false, c4 = fa
       </div> :
         <div className={'flex justify-center items-center p-1 ' + (turn ? ' rotate-90 ' : ' ') + className}>
           <div className='relative'>
-            <img className='absolute  -right-[85%]' src={c1 ? fullChair : freeChair} alt="Top right chair" />
+            <img className='absolute  -right-[25%]' src={c1 ? fullChair : freeChair} alt="Top right chair" />
             <img className='opacity-0' src={freeChair} alt="container chair" />
           </div>
-          <div className='px-2 z-20'>
+          <div className=' z-20'>
             <img className='mt-2' src={tableSmall} alt="Table" />
           </div>
           <div className='relative'>
-            <img className='absolute  -left-[45%]' src={c2 ? fullChair : freeChair} alt="Top left chair" style={{ transform: 'rotateY(180deg)' }} />
+            <img className='absolute  -left-[25%]' src={c2 ? fullChair : freeChair} alt="Top left chair" style={{ transform: 'rotateY(180deg)' }} />
             <img className='opacity-0' src={freeChair} alt="container chair" />
           </div>
         </div>
